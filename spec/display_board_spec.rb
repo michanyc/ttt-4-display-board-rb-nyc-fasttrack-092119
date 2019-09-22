f7e8beb50416fa1dfa71ef74e44bbac46e9c94c3
Michael Gerland <mr.gerland@gmail.com>
2019-09-22 12:51:12 +0000

    Automatically backed up by Learn

--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -188,3 +188,16 @@ describe "#display_board in 'lib/display_board.rb" do
     end
   end
 end
+
+board = [" "," "," "," ","X"," "," "," "," "]
+
+def dispaly_board(board)
+  puts " {bord[0]} , {board[1]} , {board[2] }"
+  puts "-----------"
+  puts " {bord[3]} , {board[4]} , {board[5] }"
+  puts "-----------"
+  puts " {bord[6]} , {board[7]} , {board[8] }"
+  puts "-----------"
+end
+
+display_board(board)
